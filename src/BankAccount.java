88b35dea70c4abf1c6db8000cebbedf60b3bbcce
LLParis <sirlondonparis@gmail.com>
2025-02-14 22:08:35 -0800

    Initial Commit

---
+++ src/BankAccount.java
@@ -0,0 +1,19 @@
+/*
+* BankAccount: This Java program represents a Bank application with fields for:
+*  the functionality to:
+* withdraw, deposit, and calculate monthly interest.
+ */
+
+import java.util.*;
+
+public class BankAccount {
+    // ========================= Fields =========================
+    private int accountID; // Unique account identifier
+    private String CustomerName; // Name of the customer
+    private double balance = 0;
+    private double annualInterestRate = 0;
+    private Date dateCreated;
+    public static void main(String[] args) {
+
+    }
+}
